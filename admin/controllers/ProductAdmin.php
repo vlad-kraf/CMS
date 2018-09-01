d0ffd652694eedb9d6d993c1f9afdf9334ebb7a4
vlad-kraf <vladkravchenkomail@gmail.com>
2018-09-01 08:03:35 +0300

    files_upload

--- admin/controllers/ProductAdmin.php
+++ admin/controllers/ProductAdmin.php
@@ -19,6 +19,40 @@ class ProductAdmin extends CoreAdmin
                 $product->url = $request->post('url');
             }
 
+            if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['save'])) {
+                $files = reset($_FILES);
+
+                $filename = $files['name'];
+                    $path = $files['tmp_name'];
+
+                $base = pathinfo($filename, PATHINFO_FILENAME);
+                $ext = pathinfo($filename,PATHINFO_EXTENSION);
+
+
+                $md5 = md5(rand(100,100500));
+                $len = strlen($md5);
+                $tm = '';
+                for ($x = 0; $x < $len; $x+=4) {
+                    $tm .= $md5[$x+rand(1,4)];
+                }
+                $name = $base.'_'.$tm.'.'.$ext;
+                unset($tm);
+
+                $res = move_uploaded_file($path,'\uploads'.$name);
+
+
+                if($res) {
+                    $product->filename = $name;
+                    print_r ($files);
+                    print_r ($product);
+                } else {
+                     Echo 'upload_error';
+                }
+
+            }
+
+
+
             if($request->post('id','integer')) {
                 //обновляем товар
                 $id = $products->updateProduct($request->post('id','integer'),$product);
